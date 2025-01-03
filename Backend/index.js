3861ca4166228cac5bea8e4b249d71f41e7eec75
Isuru <it21041648@my.sliit.lk>
2025-01-03 18:12:36 +0530

    MongoDb connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import connectDB from "./Config/db.js";
 
 const port = process.env.PORT || 3000;
+
+await connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
